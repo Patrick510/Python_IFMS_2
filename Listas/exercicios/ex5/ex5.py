667614bfc0e5e1b92f8265e3c7119a913122d933
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-03-13 16:44:30 -0400

    Exercicio 4??

--- Listas/exercicios/ex5/ex5.py
+++ Listas/exercicios/ex5/ex5.py
@@ -1,1 +1,25 @@
 """Ler dois arquivos de texto diferentes. Escrever as palavras presentes em pelo menos um dos arquivos, sem duplicatas."""
+
+def ler_arquivo(nome_arquivo):
+  with open(nome_arquivo, 'r') as arquivo:
+    conteudo = arquivo.read()
+    palavras = conteudo.split()
+    return set(palavras)
+
+def verificar_arquivo(arquivo1, arquivo2):
+  palavras_arquivo1 = ler_arquivo(arquivo1)
+  palavras_arquivo2 = ler_arquivo(arquivo2)
+  palavras_presentes = palavras_arquivo1.union(palavras_arquivo2)
+  return palavras_presentes
+
+arquivos = []
+
+for indice in range(2):
+  arquivo = f"{str(input(f'Nome {indice+1}° arquivo:'))}.txt"
+  arquivos.append(arquivo)
+
+palavras_presentes = set()
+
+palavras = verificar_arquivo(arquivos[0], arquivos[1])
+
+print(palavras)
