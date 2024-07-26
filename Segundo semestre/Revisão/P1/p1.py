c371197ec92f1ad945c502e30794e6c5d876fc06
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-07-25 21:46:17 -0400

    :25-07: Prova 1 refeita

---
+++ Segundo semestre/Revisão/P1/p1.py
@@ -0,0 +1,47 @@
+# biblioteca = [
+#   ("Python for Beginners", "John Smith", 2020, 300),
+#   ("Data Science Essentials", "Jane Doe", 2019, 450),
+#   ("History of Science", "Robert Johnson", 2018, 250),
+#   ("Artificial Intelligence in Practice", "Alice Williams", 2021, 380),
+#   ("Literary Classics", "David Brown", 2017, 500)
+# ]
+
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+  title: str
+  autor: str
+  year: int
+  number_pages: int
+  
+library = []
+  
+def add_book(title, autor, year, number):
+  new_book = Book(title, autor, year, number) 
+  library.append(new_book)
+
+def remove_book(title, autor):
+  global library
+  library = [book for book in library if book.title != title or book.autor != autor]
+
+def search_book_autor(autor):
+  return [book for book in library if book.autor == autor]
+
+def books_published_year(year):
+  return [books for books in library if books.year == year]
+
+def average_number_pages():
+  num_pages = sum(books.number_pages for books in library)
+  return num_pages/len(library)
+
+def books_more_then(number_page):
+  return [books for books in library if books.number_pages == number_page]
+
+add_book("Python for Beginners", "John Smith", 2020, 380)
+add_book("Artificial Intelligence in Practice", "Alice Williams", 2020, 380)
+remove_book("Python for Beginners", "John Smith")
+print(search_book_autor("John Smith"))
+print(books_published_year(2020))
+print(average_number_pages())
+print(books_more_then(380))
---
+++ Segundo semestre/Revisão/Rembr/relembrar.py
@@ -0,0 +1,64 @@
+list1 = ["Computer", "Printer", "TV", "Camera", 89, 30.8]
+list1[0] = "PC"
+# print(list1)
+# print()
+
+set1 = set(["Computer", "Printer", "TV","Camera", 89, 30.8])
+# print(set1)
+# print()
+
+dict1 = {
+  1:["Computer", "Printer", "TV","Camera", 89, 30.8],
+  2:["Computer2", "Printer2", "TV2","Camera2", 892, 30.82],
+  3:["Nothing"]
+}
+dict1[1][3] = "None"
+dict1[1].remove("None")
+del dict1[3]
+
+tuple1 = [
+  ("Computer", "Printer", "TV", "Camera", 89, 30.8),
+  ("Computer2", "Printer2", "TV2","Camera2", 892, 30.82),
+  ("Nothing")
+]
+
+#Retirando apenas o elemento "TV da tupla"
+new_tuple1 = tuple(item for item in tuple1[0] if item != "TV")
+
+new_tuple1 = [new_tuple1, tuple1[1], tuple1[2]]
+
+#Retirando uma lista inteira
+new_tuple1 = tuple(item for item in tuple1 if item != tuple1[0])
+
+#Remover qualquer tupla que contenha "Nothing"
+new_tuple1 = tuple(item for item in tuple1 if "Nothing" not in item)
+
+tuple2 = [
+  ("Computer", "Printer", "TV", "Camera", 89, 30.8),
+  ("Computer2", "Printer2", "TV2","Camera2", 892, 30.82),
+  ("Nothing")
+]
+
+new_tuple2 = tuple(item for item in tuple1[0] if item != "TV")
+tuple2 = [new_tuple2, tuple2[1], tuple2[2]]
+print("Após remover 'TV' da primeira sub-tupla")
+print(tuple2)
+
+print()
+
+tuple2 = [item for item in tuple2 if item != tuple2[0]]
+print("Após remover a primeira sub-tupla inteira")
+print(tuple2)
+print()
+
+tuple2 = [item for item in tuple2 if "Nothing" not in item]
+print("Após remover qualquer sub-tupla que contenha 'Nothing'")
+print(tuple2)
+
+
+new_list = ["NewItem1", "NewItem2", "NewItem3"]
+tuple2.append(tuple(new_list))
+print("Após adicionar a lista como nova sub-tupla")
+print(tuple2)
+
+
